007c97706f4d08d3deb5548fc13bac927ff6a9fe
tanguy richard <tanguy.richard.tar@gmail.com>
2025-06-12 11:56:11 -0400

    testing open ai

--- pages/Explanation.py
+++ pages/Explanation.py
@@ -120,16 +120,21 @@ else:
                      <br>
                      <b>Why the {mbti_14} ?<br> </b>
                      {explanation_14} <br>
-                     <br>
                      </div>""", unsafe_allow_html=True)
-
-        st.markdown(f'<div class="green-opaque-box"><b>Why the {mbti_13} ?<br> </b> {explanation_13}</div>', unsafe_allow_html=True)
-        st.markdown(f'<div class="green-opaque-box"><b>Why the {mbti_14} ?<br> </b> {explanation_14}</div>', unsafe_allow_html=True)
 
     with col2:
         st.markdown(f'<div class="green-opaque-box"><b>Tweet from Person 2:</b> {mbti_2} <br> with a confidence of {conf_2}%</div>', unsafe_allow_html=True)
-        st.markdown(f'<div class="green-opaque-box"><b>Why the {mbti_21} ?<br> </b> {explanation_21}</div>', unsafe_allow_html=True)
-        st.markdown(f'<div class="green-opaque-box"><b>Why the {mbti_22} ?<br> </b> {explanation_22}</div>', unsafe_allow_html=True)
-        st.markdown(f'<div class="green-opaque-box"><b>Why the {mbti_23} ?<br> </b> {explanation_23}</div>', unsafe_allow_html=True)
-        st.markdown(f'<div class="green-opaque-box"><b>Why the {mbti_24} ?<br> </b> {explanation_24}</div>', unsafe_allow_html=True)
+        st.markdown(f"""<div class="green-opaque-box">
+            <b>Why the {mbti_21} ?</b> <br>
+                {explanation_21} <br>
+                <br>
+                <b>Why the {mbti_22} ?<br> </b>
+                {explanation_22} <br>
+                <br>
+                <b>Why the {mbti_23} ?<br> </b>
+                {explanation_23} <br>
+                <br>
+                <b>Why the {mbti_24} ?<br> </b>
+                {explanation_24} <br>
+                </div>""", unsafe_allow_html=True)
     st.markdown("---")
